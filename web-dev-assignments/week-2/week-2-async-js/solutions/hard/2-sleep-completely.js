e083da03de94f78a7c77a3925cf3d53342144d8d
Tushar Asodariya <tusharasodariya1@gmail.com>
2025-07-29 23:25:02 +0530

    Remove unused promise-based sleep and wait functions from week 2 assignments

--- web-dev-assignments/week-2/week-2-async-js/solutions/hard/2-sleep-completely.js
+++
@@ -1,9 +0,0 @@
-function sleep(milliseconds) {
-    return new Promise((resolve) => {
-        let startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliseconds);
-        resolve();
-    });
-}
-
-module.exports = sleep;
